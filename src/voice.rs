7e616b6e2bd80e4798b554549c0994990e1efa0d
Dan Nixon <dan@dan-nixon.com>
2024-05-29 01:34:48 +0000

    Add remaining functionality

--- src/voice.rs
+++ src/voice.rs
@@ -1,4 +1,5 @@
 use crate::jambonz::{Say, SaySynthesizer, Verb};
+use chrono::{DateTime, Datelike, Duration, FixedOffset};
 
 pub(crate) fn speak(text: &str) -> Say {
     Say {
@@ -14,4 +15,28 @@ pub(crate) fn speak(text: &str) -> Say {
 
 pub(crate) fn speak_verb(text: &str) -> Verb {
     Verb::Say(speak(text))
+}
+
+pub(crate) fn format_timestamp_relative_to(
+    timestamp: DateTime<FixedOffset>,
+    now: DateTime<FixedOffset>,
+) -> String {
+    if timestamp.day() == now.day() {
+        timestamp.format("%H:%M")
+    } else {
+        timestamp.format("%A %H:%M")
+    }
+    .to_string()
+}
+
+pub(crate) fn format_duration(duration: Duration) -> String {
+    let hours = if duration.num_hours() > 0 {
+        format!("{} hours", duration.num_hours())
+    } else {
+        "".to_string()
+    };
+
+    let minutes = format!("{} minutes", duration.num_minutes() % 60);
+
+    format!("{hours}{minutes}")
 }
